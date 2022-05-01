e6c91184c071adb39fb5a1d17a871373084bb9a2
farProgrammer <72961644+farProgrammer@users.noreply.github.com>
2022-04-30 21:42:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
-  "mongodb+srv://admin:admin123@cluster0.ti0ez.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://username:password@cluster0.ti0ez.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
